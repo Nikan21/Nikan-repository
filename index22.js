f6c97ee37c7510ee8c508b2aa270f167ca10e346
Dima Nikanorov <dimonnikanorov2@gmail.com>
2021-07-22 18:30:21 +0300

    Finish task

--- index22.js
+++ index22.js
@@ -19,4 +19,14 @@ const country3 = 'GreatBritain';
 const str1 = `${city1} is a capital of ${country3}`;
 const str2 = `${city2} is a capital of ${country1}`;
 const str3 = `${city3} is a capital of ${country2}`;
+console.log(str1);
+console.log(str2);
+console.log(str3);
+
+/*Test 3*/
+const test = ['London', 'Minsk', 'NewYork'];
+const test1 = ['Belorussia', 'USA', 'GreatBritain'];
+const str4 = `${test[0]} is a capital of ${test1[2]}`;
+const str5 = `${test[1]} is a capital of ${test1[0]}`;
+const str6 = `${test[2]} is a capital of ${test1[1]}`;
 console.log([str1, str2, str3]);
