59043d2b56789e19a07b1d9c5e551d1c374b82d3
Dima Nikanorov <dimonnikanorov2@gmail.com>
2021-07-22 18:24:16 +0300

    Add index 22

---
+++ index22.js
@@ -0,0 +1,22 @@
+/*1*/
+const country = 'Ukraine';
+const str = `I live in ${country}`;
+console.log(str);
+
+/*2*/
+const firstname = 'Dima';
+const id = 21;
+const string = `${firstname}_${id}`;
+console.log(string);
+
+/*3*/
+const city1 = 'London';
+const city2 = 'Minsk';
+const city3 = 'NewYork';
+const country1 = 'Belorussia';
+const country2 = 'USA';
+const country3 = 'GreatBritain';
+const str1 = `${city1} is a capital of ${country3}`;
+const str2 = `${city2} is a capital of ${country1}`;
+const str3 = `${city3} is a capital of ${country2}`;
+console.log([str1, str2, str3]);
